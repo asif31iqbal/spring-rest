abe2c3ea088045c360983af5a3bb59447890339c
Asif Iqbal <asif.31.iqbal@gmail.com>
2016-06-01 18:35:11 -0400

    Trying XML as response besides JSON

--- src/main/java/com/asif/app/Application.java
+++ src/main/java/com/asif/app/Application.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 
 @SpringBootApplication(scanBasePackages="com.asif")
 public class Application extends SpringBootServletInitializer {
@@ -15,5 +22,21 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }   
+    
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(String.class, new StringSerializer());
+        ObjectMapper mapper = new ObjectMapper()
+            .registerModule(simpleModule);
+        converter.setObjectMapper(mapper);
+        return converter;
+    }
+
+    @Bean
+    public Jaxb2RootElementHttpMessageConverter jaxbConverter() {
+        return new Jaxb2RootElementHttpMessageConverter();
     }
 }
--- src/main/java/com/asif/controller/BookController.java
+++ src/main/java/com/asif/controller/BookController.java
@@ -1,7 +1,13 @@
 package com.asif.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,14 +21,42 @@ import com.asif.model.Book;
 public class BookController {
 	private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping(value = "/create", method = RequestMethod.GET)
-	public Book getBook(@RequestParam(value="name", defaultValue="Test Book") String name,
-			@RequestParam(value="author", defaultValue="Test Author") String author) {
-		return new Book(counter.incrementAndGet(), name, author);
+	Map<Long, Book> map = new HashMap<Long, Book>();
+	
+	@RequestMapping(value = "/book", method = RequestMethod.GET)
+	public Book getBook(@RequestParam(value="name", defaultValue="Test Book") String name) {
+		for(Entry<Long, Book> entry : map.entrySet()){
+		    if(entry.getValue().getName().equals(name)){
+		        return entry.getValue();
+		    }
+		}
+		
+		return null;
 	}
 	
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public Book getBook2(@PathVariable String name) {
-		return new Book(counter.incrementAndGet(), name, "Test Author");
-	}
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Book getBook(@PathVariable Long id) {
+        return map.get(id);
+    }
+	
+//	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+//	public Book getBook2(@PathVariable String name) {
+//		return new Book(counter.incrementAndGet(), name, "Test Author");
+//	}
+//	
+//	@RequestMapping(method = RequestMethod.GET)
+//    public Book getBook3() {
+//        return new Book(counter.incrementAndGet(), "Default name", "Default Author");
+//    }
+	
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+    public void createBook() {
+        long id = counter.incrementAndGet();
+        map.put(id, new Book(id, "Default name", "Default Author"));
+    }
+	
+	@RequestMapping(method = RequestMethod.GET, produces={MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_XML_VALUE})
+    public List<Book> getBooks() {
+        return new ArrayList<Book>(map.values());
+    }
 }
